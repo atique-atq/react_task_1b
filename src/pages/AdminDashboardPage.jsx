d6b4e4a3c4fbc315003f8527e39ff2290af06e69
atique-atq <atique.atq@gmail.com>
2023-05-04 02:09:01 +0600

    started dashboard

--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -3,8 +3,16 @@ import React from "react";
 const AdminDashboardPage = () => {
   return (
     <>
-      <div className="w-full flex justify-center items-center text-7xl h-screen text-gray-700 ">
-        Dashboard
+      <div className="h-screen text-gray-700 bg-black
+]">
+        <div className="pt-8 px-24 flex justify-between">
+          <div className="text-white text-5xl font-bold">APP</div>
+          <button class="rounded-full px-8 bg-[#9BFF00]">Logout</button>
+        </div>
+
+        <div className=" h-full w-full flex justify-center items-center text-7xl">
+          <h1 className="text-white">Dashboard</h1>
+        </div>
       </div>
     </>
   );
